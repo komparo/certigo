efe0f9f4efec93309edb082ee8a4d4e5dba87ff2
zouter <wouter.saelens@gmail.com>
2018-10-17 17:32:43 +0200

    tests shouldn't produce output

--- tests/testthat/test_workflow_animals.R
+++ tests/testthat/test_workflow_animals.R
@@ -15,7 +15,7 @@ dir_copy(system.file("testdata/workflow_animals", package = "certigo"), tempdir)
 setwd(paste0(tempdir, "/workflow_animals"))
 
 # build the docker image
-processx::run("docker", c("build", "-t", "certigo/plot_animal_cuteness", system.file('testdata/workflow_animals/containers/plot_animal_cuteness/', package = 'certigo')), echo = T)
+processx::run("docker", c("build", "-t", "certigo/plot_animal_cuteness", system.file('testdata/workflow_animals/containers/plot_animal_cuteness/', package = 'certigo')), echo = F)
 
 # some testing functions
 expect_rerun <- function(x) {expect_output(x, "^.*Finished$", info = "Expected a rerun")}
@@ -71,11 +71,11 @@ expect_true(file_exists(plot_animal_cuteness$outputs[[1]]$string))
 
 # no input -> error + deleted output
 aggregate_animal_cuteness$outputs[[1]]$delete()
-expect_error(plot_animal_cuteness$start_and_wait())
+expect_error(capture_output(plot_animal_cuteness$start_and_wait()))
 expect_false(file_exists(plot_animal_cuteness$outputs[[1]]$string))
 
 # input -> rerun
-aggregate_animal_cuteness$start_and_wait()
+expect_rerun(aggregate_animal_cuteness$start_and_wait())
 expect_rerun(plot_animal_cuteness$start_and_wait())
 
 # test some other calls
