a8fd6b4d484ae39d2eacb5116b96ea805d8ee062
Wouter Saelens <wouter.saelens@gmail.com>
2018-11-26 14:43:55 +0100

    add test for workflow plotting

--- inst/testdata/workflow_animals/scripts/determine_animal_cuteness.R
+++ inst/testdata/workflow_animals/scripts/determine_animal_cuteness.R
@@ -4,7 +4,7 @@ design <- jsonlite::read_json(inputs[[1]], simplifyVector = TRUE)
 
 animal_cuteness <- tibble(
   animal = design$animal,
-  cuteness = rgamma(design$n_animals, 1, 1/design$cuteness_mean)
+  cuteness = rgamma(design$n_animals, 1, 1 / design$cuteness_mean)
 )
 
 readr::write_csv(animal_cuteness, outputs[[1]])
